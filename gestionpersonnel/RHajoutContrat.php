a55ab94caad3ec5690c21be859fe81b8bc891b02
Gilles Van Remortel <121046307+givaremo@users.noreply.github.com>
2023-12-27 17:20:26 +0100

    Add files via upload

--- gestionpersonnel/RHajoutContrat.php
+++ gestionpersonnel/RHajoutContrat.php
@@ -9,6 +9,57 @@ $estDirecteurFinancier = $_SESSION['estDirecteurFinancier'];
 $nomTypeEmploye = $_SESSION['nomTypeEmploye'];
 
 
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submitContrat'])) {
+    // Récupérer les données du formulaire
+    $datedebutcontrat = $_POST['datedebutcontrat'];
+    $datefincontrat = $_POST['datefincontrat'];
+    $salaire = $_POST['salaire'];
+    $fkidemploye = $_POST['fkidemploye'];
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "gestionpersonnel";
+    
+// Créer une connexion à la base de données
+    $connexion = new mysqli($servername, $username, $password, $dbname);
+
+    // Vérifier la connexion
+    if ($connexion->connect_error) {
+        die("La connexion à la base de données a échoué : " . $connexion->connect_error);
+    }
+
+// Exemple de requête MySQL utilisant la procédure stockée
+$query = "CALL usp_ajoutContrat(?, ?,?, ?)";
+$stmt = $connexion->prepare($query);
+
+// Vérifier si la préparation de la requête a réussi
+if ($stmt === FALSE) {
+    die("Erreur lors de la préparation de la requête : " . $connexion->error);
+}
+
+
+// Binder les paramètres à la requête
+$stmt->bind_param("ssdi", $datedebutcontrat, $datefincontrat, $salaire, $fkidemploye);
+
+// Exécuter la requête
+if ($stmt->execute()) {
+	$resexecution = "Contrat ajouté avec succès";
+} else {
+	$resexecution = "Erreur lors de l'ajout de la note de frais : " . $stmt->error;
+}
+
+// Fermer la requête
+$stmt->close();
+}
+
+
+
+
+
+
+
 echo '
 <!DOCTYPE html>
 <html lang="fr">
@@ -34,16 +85,84 @@ echo '<a href="deconnexion.php" class="logout-link">Déconnexion</a>';
 echo '<br></div>' ;
 
 
-echo        '<h2>Ajout Contrat</h2>';
+echo        '<h2>Ajout contrat</h2>';
 
 
 
 if ($estRH==1)
 {
-echo '<div class="submenu-container">
-        </div>' ;
 
 
+	// Connexion à la base de données
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "gestionpersonnel";
+
+	$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+	// Vérifier la connexion
+	if ($conn->connect_error) {
+	    die("Échec de la connexion à la base de données : " . $conn->connect_error);
+	}
+
+	$sqlemploye = "call usp_selectEmploye";
+    $resultemploye = $conn->query($sqlemploye);
+
+
+
+
+
+echo '<div class="submenu-container">' ;
+
+
+echo '<form method="post" action="">
+	<table>
+	
+	<tr>
+		<td>Employé</td>
+		<td>
+			<select type="text" name="fkidemploye" required>';
+            // Génération des options de la liste déroulante
+			if ($resultemploye !== false && $resultemploye->num_rows > 0) {
+                while($row = $resultemploye->fetch_assoc()) {
+                    echo "<option value='" . $row["idemploye"] . "'>" . $row["idemploye"] . "-" . $row["prenomemploye"] . " " . $row["nomemploye"] . " / " . $row["nomdepartement"] . "</option>";
+                }
+            } else {
+                echo "<option value=''>Aucun élément trouvé</option>";
+            }
+			echo '</select>
+		</td>
+	</tr>
+	
+	<tr>
+        <td>Salaire</td>
+		<td><input type="floatval" name="salaire" required></td>
+	</tr>	
+	
+	<tr>
+        <td>Date début contrat</td>
+		<td><input type="date" name="datedebutcontrat" required></td>
+	</tr>
+	<tr>
+        <td>Date fin contrat</td>
+		<td><input type="date" name="datefincontrat" required value="2099-12-31"></td>
+	</tr>
+
+	</table>
+        <br>
+	<input type="submit" name="submitContrat" value="Création contrat">
+	<br><br>';
+
+	if (isset($resexecution)) {
+	    echo "<h4> $resexecution </h4>";
+	}	
+	echo '
+      </form>';
+
+
+echo  '</div>' ;
 }
 
 
